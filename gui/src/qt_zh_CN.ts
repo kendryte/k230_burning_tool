c58546f97f8db1d7a1a941fbbe02412b2b39b9b7
xiaenling <xiaenling@canaan-creative.com>
2023-04-21 15:54:02 +0800

    Update.

--- gui/src/qt_zh_CN.ts
+++ gui/src/qt_zh_CN.ts
@@ -1,78 +1,402 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en" sourcelanguage="zh_CN">
+<TS version="2.1" language="zh_CN">
 <context>
-    <name>MainWindow</name>
+    <name></name>
     <message>
-        <location filename="MainWindow.ui" line="14"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="180"/>
-        <source>MainWindow</source>
+        <location filename="common/BurnLibrary.cpp" line="68"/>
+        <source>错误</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="45"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="187"/>
-        <source>系统镜像文件</source>
+        <location filename="common/BurnLibrary.cpp" line="69"/>
+        <source>无法初始化读写功能:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="55"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="188"/>
-        <source>选择</source>
+        <location filename="common/BurnLibrary.cpp" line="69"/>
+        <source>error kind: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="64"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="189"/>
+        <location filename="common/BurnLibrary.cpp" line="69"/>
+        <source>code: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/BurningProcess.cpp" line="18"/>
+        <source>无法打开系统镜像文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/BurningProcess.cpp" line="56"/>
+        <source>下载中</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/BurningProcess.cpp" line="78"/>
+        <source>完成</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/BurningProcess.cpp" line="121"/>
+        <source>用户取消操作</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/K230BurningProcess.cpp" line="31"/>
+        <source>等待设备上电</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/K230BurningProcess.cpp" line="38"/>
+        <source>写入USB LOADER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/K230BurningProcess.cpp" line="44"/>
+        <source>等待USB ISP启动</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/K230BurningProcess.cpp" line="122"/>
+        <source>设备断开</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/MyException.h" line="10"/>
+        <source>没有发生错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/UpdateChecker.cpp" line="26"/>
+        <source>更新出错</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/UpdateChecker.cpp" line="31"/>
+        <source>正在检查更新</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/UpdateChecker.cpp" line="44"/>
+        <location filename="common/UpdateChecker.cpp" line="45"/>
+        <source>无法检查更新: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/UpdateChecker.cpp" line="55"/>
+        <source>←发现更新</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/UpdateChecker.cpp" line="57"/>
+        <source>没有更新</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="version.cpp" line="7"/>
+        <source>version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="version.cpp" line="10"/>
+        <source>Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="version.cpp" line="12"/>
+        <source>Release</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="version.cpp" line="15"/>
+        <source>local</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BurningControlWindow</name>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="26"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="38"/>
+        <source>镜像文件选择</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="49"/>
+        <source>选择文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="62"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="75"/>
+        <source>设备选项</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="87"/>
+        <source>目标设备</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="130"/>
+        <source>自动烧录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="159"/>
+        <source>设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/BurningControlWindow.ui" line="204"/>
         <source>开始烧录</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="21"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="184"/>
-        <source>自动探测新设备并烧录</source>
+        <location filename="widgets/BurningControlWindow.cpp" line="47"/>
+        <location filename="widgets/BurningControlWindow.cpp" line="63"/>
+        <source>请先打开系统镜像！！！</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="31"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="191"/>
-        <source>手动烧录</source>
+        <location filename="widgets/BurningControlWindow.cpp" line="81"/>
+        <source>打开系统镜像</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="85"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="190"/>
-        <source>TextLabel</source>
+        <location filename="widgets/BurningControlWindow.cpp" line="115"/>
+        <source>file size: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="107"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="192"/>
-        <source>批量烧录</source>
+        <location filename="widgets/BurningControlWindow.cpp" line="126"/>
+        <source>file not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BurningProcess</name>
+    <message>
+        <location filename="common/BurningProcess.cpp" line="70"/>
+        <source>设备写入失败，地址: 0x</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="125"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="193"/>
+        <location filename="common/BurningProcess.cpp" line="129"/>
+        <source>用户主动取消</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>K230BurningProcess</name>
+    <message>
+        <location filename="common/K230BurningProcess.cpp" line="35"/>
+        <source>设备上电超时</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/K230BurningProcess.cpp" line="41"/>
+        <source>写入USB LOADER失败</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="common/K230BurningProcess.cpp" line="48"/>
+        <source>ISP启动超时</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoggerWindow</name>
+    <message>
+        <location filename="widgets/LoggerWindow.cpp" line="71"/>
+        <source>Auto Scroll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/LoggerWindow.cpp" line="75"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.ui" line="14"/>
+        <source>Kendryte buring tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="145"/>
         <source>文件(&amp;F)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="131"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="194"/>
+        <location filename="MainWindow.ui" line="156"/>
         <source>关于(&amp;A)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="141"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="181"/>
-        <source>打开官网</source>
+        <location filename="MainWindow.ui" line="166"/>
+        <source>检查更新</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="146"/>
-        <location filename="../../build-vscode/gui/BurningTool_autogen/include/ui_MainWindow.h" line="182"/>
+        <location filename="MainWindow.ui" line="179"/>
+        <source>打开主页</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="191"/>
         <source>退出(&amp;Q)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="194"/>
+        <source>退出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="206"/>
+        <source>保存日志(&amp;S)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="209"/>
+        <source>保存日志</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="212"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="223"/>
+        <source>更详细显示日志</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="231"/>
+        <source>调试数据传输详情</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="236"/>
+        <source>下载最新版本</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="241"/>
+        <source>展开日志窗口</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="116"/>
+        <source>日志保存路径</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="117"/>
+        <source>Log File (*.html);;All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="35"/>
+        <source>程序选项</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="56"/>
+        <source>成功完成后自动确认</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="88"/>
+        <location filename="widgets/SettingsWindow.ui" line="141"/>
+        <location filename="widgets/SettingsWindow.ui" line="184"/>
+        <source>秒</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="100"/>
+        <source>禁止联网（无更新提示）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="109"/>
+        <source>自动确认手动添加的任务</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="152"/>
+        <source>失败后也自动确认（无错误提示）</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="201"/>
+        <source>线程数量上限</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="255"/>
+        <source>保存</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SettingsWindow.ui" line="275"/>
+        <source>恢复默认设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SingleBurnWindow</name>
+    <message>
+        <location filename="widgets/SingleBurnWindow.ui" line="83"/>
+        <source>TITLE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SingleBurnWindow.ui" line="109"/>
+        <source>重试</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SingleBurnWindow.ui" line="123"/>
+        <location filename="widgets/SingleBurnWindow.cpp" line="195"/>
+        <source>确认</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SingleBurnWindow.ui" line="137"/>
+        <source>中断</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SingleBurnWindow.cpp" line="87"/>
+        <source>完成！</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SingleBurnWindow.cpp" line="100"/>
+        <source>错误: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widgets/SingleBurnWindow.cpp" line="116"/>
+        <source>正在取消</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
