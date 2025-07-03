9391d3637eaae589d77f09f6ee80085a80c79862
kendryte747 <kendryte747@gmail.com>
2025-07-03 17:32:32 +0800

    Update

--- release.sh
+++ release.sh
@@ -45,7 +45,7 @@ mkdir -p build
 export CI=1
 
 # Run cmake config
-qt-cmake ./project -G Ninja -B ./build -DBUILD_FOR_AVALON_NANO3=$BUILD_FOR_AVALON_NANO3
+qt-cmake . -G Ninja -B ./build -DBUILD_FOR_AVALON_NANO3=$BUILD_FOR_AVALON_NANO3
 
 # Build
 cmake --build ./build
@@ -54,26 +54,5 @@ cmake --build ./build
 cmake --install ./build
 
 ls -alh ./build
-
-# Package output
-if [[ "$OS" == "windows" ]]; then
-  if [[ "$VARIANT" == "avalon3" ]]; then
-    BIN_NAME="Avalon_Home_Series_Firmware_Upgrade_Tool.exe"
-  else
-    BIN_NAME="K230BurningTool.exe"
-  fi
-  cp "./build/dist/$BIN_NAME" "./build/$BIN_NAME"
-  ZIP_NAME="K230BurningTool-Windows-$VARIANT-$REVISION.zip"
-  zip -j "$ZIP_NAME" "./build/$BIN_NAME"
-else
-  # macOS or Linux
-  if [[ "$VARIANT" == "avalon3" ]]; then
-    APPIMAGE="Avalon_Home_Series_Firmware_Upgrade_Tool.AppImage"
-  else
-    APPIMAGE="K230BurningTool-x86_64.AppImage"
-  fi
-  ZIP_NAME="K230BurningTool-$OS-$VARIANT-$REVISION.zip"
-  zip -j "$ZIP_NAME" "./build/$APPIMAGE"
-fi
 
 echo "Build complete! Artifact: $ZIP_NAME"
